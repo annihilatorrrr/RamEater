bb87ac499d9c941b4393616c4e9314d6722fbece
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-03-19 18:37:25 +0530

    Done.

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    // Get total memory in bytes
+    var sysinfo syscall.Sysinfo_t
+    if err := syscall.Sysinfo(&sysinfo); err != nil {
+        fmt.Println("Failed to get system info:", err)
+        os.Exit(1)
+    }
+    total := sysinfo.Totalram * uint64(sysinfo.Unit)
+    // Calculate the amount of memory to use (4% of total)
+    use := total / 4
+    // Allocate memory
+    memory := make([]byte, use)
+    fmt.Println("Done!")
+    if memory == nil {
+        fmt.Println("Failed to allocate more memory!")
+        for {
+            time.Sleep(1000)
+        }
+    }
+    fmt.Println("Ok!")
+}
