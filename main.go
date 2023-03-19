597c375851bb657e2468f7dfe5b6c77beca8a1d6
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-03-19 19:23:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "syscall"
-    "time"
+	"fmt"
+	"os"
+	"syscall"
+	"time"
 )
 
 func main() {
-    // Get total memory in bytes
-    var sysinfo syscall.Sysinfo_t
-    if err := syscall.Sysinfo(&sysinfo); err != nil {
-        fmt.Println("Failed to get system info:", err)
-        os.Exit(1)
-    }
-    total := sysinfo.Totalram * uint64(sysinfo.Unit)
-    // Calculate the amount of memory to use (4% of total)
-    use := total / 4
-    // Allocate memory
-    memory := make([]byte, use)
-    fmt.Println("Done!")
-    if memory == nil {
-        fmt.Println("Failed to allocate more memory!")
-        for {
-            time.Sleep(1000)
-        }
-    }
-    fmt.Println("Ok!")
+	// Get total memory in bytes
+	var sysinfo syscall.Sysinfo
+	if err := syscall.Sysinfo(&sysinfo); err != nil {
+		fmt.Println("Failed to get system info:", err)
+		os.Exit(1)
+	}
+	total := sysinfo.Totalram * uint64(sysinfo.Unit)
+	// Calculate the amount of memory to use (4% of total)
+	use := total / 4
+	// Allocate memory
+	if memory := make([]byte, use); memory == nil {
+		fmt.Println("Failed to allocate more memory!")
+	} else {
+		fmt.Println("Done!")
+		for {
+			time.Sleep(1000)
+		}
+	}
 }
