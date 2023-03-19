b1683cdecdad9c6e4493726d486e82dfac2d29e7
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-03-19 19:00:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
 
 int main() {
+    long long total, use;
+ #ifdef _WIN32
+  MEMORYSTATUSEX status;
+  status.dwLength = sizeof(status);
+  GlobalMemoryStatusEx(&status);
   // Get total memory in bytes
-  long total = sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
+  total = status.ullTotalPhys;
+  #else
+    // Get total memory in bytes
+  total = sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
+  #endif
   // Calculate the amount of memory to use (10% of total)
-  long use = total / 10;
+  use = total / 10;
   // Allocate memory
-  char *memory = malloc(use);
-  // Check if allocation was successful
-  if (memory == NULL) {
-    printf_s("Failed to allocate memory\n");
-  }
+  char *memory = (char *) malloc(use);
+  // Sleep for 10 seconds
+  #ifdef _WIN32
+    Sleep(10000);
+  #else
+  sleep(10);
+  #endif
+  // Free memory
+  free(memory);
   return 0;
 }
