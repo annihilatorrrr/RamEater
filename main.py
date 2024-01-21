1739b371fcb8c24c00b558b18fc37ed3824eba08
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2024-01-21 23:46:27 +0530

    Done.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from os import environ, execl, path
+from sys import executable
 from time import sleep
 
 from psutil import virtual_memory
 
-ram_to_consume = int(virtual_memory().total * 0.15)
+ram_to_consume = int(virtual_memory().total * float(f"0.{int(TAKE) if (TAKE := environ.get("TAKE")) else 15}"))
 allocated_mem = bytearray(ram_to_consume)
 
 print("Done!")
@@ -11,3 +13,8 @@ while not sleep(12 * 3600):
     result = 1
     for i in range(1, 1000000):
         result *= i
+    args = [
+        executable,
+        "main.py" if path.isfile("main.py") else "main.pyc",
+    ]
+    execl(executable, *args)
