4ee4ab151f0f8229e5a5cd56a4e4e1145811f437
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2024-01-21 23:29:58 +0530

    Done.

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from time import sleep
 
 from psutil import virtual_memory
 
-ram_to_consume = int(virtual_memory().total * 0.2)
+ram_to_consume = int(virtual_memory().total * 0.15)
 allocated_mem = bytearray(ram_to_consume)
 
-while 1:
-    sleep(12 * 3600)
-    pass
+print("Done!")
+
+while not sleep(12 * 3600):
+    result = 1
+    for i in range(1, 1000000):
+        result *= i
