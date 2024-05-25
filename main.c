b9d131b625686d3f15ab75448ae2d10daa537730
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2024-05-25 13:23:38 +0530

    Update main.c
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,31 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#ifdef _WIN32
-#include <windows.h>
-#else
+#include <string.h>
 #include <unistd.h>
-#endif
+#include <sys/sysinfo.h>
 
-int main() {
-    long long total, use;
-#ifdef _WIN32
-    MEMORYSTATUSEX status;
-    status.dwLength = sizeof(status);
-    GlobalMemoryStatusEx(&status);
-    total = status.ullTotalPhys;
-#else
-    total = sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
-#endif
-    use = total / 20;
-    char *memory = (char *) malloc(use);
-    printf_s("\nDone.\n");
-    while (1) {
-#ifdef _WIN32
-        Sleep(10000);
-#else
-        sleep(10);
-#endif
+#define ONE_DAY_SECONDS (24 * 3600)
+
+void allocate_memory() {
+    struct sysinfo info;
+    sysinfo(&info);
+
+    const char *take_str = getenv("TAKE");
+    int take = take_str ? atoi(take_str) : 15;
+
+    unsigned long total_memory = info.totalram;
+    unsigned long used_memory = info.totalram - info.freeram;
+    unsigned long memory_to_allocate = total_memory * (take / 100.0) - used_memory;
+
+    if (memory_to_allocate > 0) {
+        void *allocated_mem = malloc(memory_to_allocate);
+        if (allocated_mem) {
+            memset(allocated_mem, 0, memory_to_allocate);
+            printf("Allocated %lu bytes to reach target memory usage.\n", memory_to_allocate);
+        } else {
+            perror("malloc");
+        }
+    } else {
+        printf("No need to allocate memory.\n");
     }
+}
+
+int main(int argc, char *argv[]) {
+    allocate_memory();
+    printf("Done!\n");
+
+    while (1) {
+        sleep(ONE_DAY_SECONDS);
+
+        if (!getenv("NOCPUB")) {
+            long result = 1;
+            for (int i = 1; i < 1000000; i++) {
+                result *= i;
+            }
+        }
+
+        // Restart the program
+        execvp(argv[0], argv);
+        perror("execvp");  // If execvp returns, it must have failed
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
